a132079a0bb11cb20658b6759ba2c19230958aa1
Pankaj Bisht <72254011+bishtpd@users.noreply.github.com>
2021-08-19 18:34:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+music = "";
+function preload(){
+    music = loadSound("music.mp3");
+}
 function setup(){
     canvas = createCanvas(650, 500);
     canvas.position(600, 250);
+    myvideo = createCapture(VIDEO);
+    myvideo.hide();
+    posenetinisialiser = ml5.poseNet(myvideo, poseNetloaded);
 
+}
+function poseNetloaded(){
+    console.log("PoseNet has been loaded.")
+}
+function draw(){
+    image(myvideo, 0, 0, 600, 500);
+}
+function playmusic(){
+    music.play();
+    music.setVolume(0.5);
+    music.rate(1);
 }
